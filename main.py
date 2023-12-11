d18914c749e0c5298df36ce3ae266b7d25b9f7b9
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-11 12:18:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import HTTPBasic, HTTPBasicCredentials, HTTPBearer, HTTPAuthorizationCredentials
-from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
 import sqlite3
 import hashlib
 import secrets
 
-
 app = FastAPI()
 
 # Permitimos los origenes para conectarse
 origins = [
-    "http://0.0.0.0:8080",  
-    "http://localhost:8080",  
-    "http://127.0.0.1:8080", 
-    "https://frontend-contactos-bloqueo-49a8fc94c0ff.herokuapp.com",
+    "http://0.0.0.0:8080",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "https://frontend-contactos-bloqueo-49a8fc94c0ff.herokuapp.com/",
 ]
 
 # Agregamos las opciones de origenes, credenciales, métodos y headers
@@ -141,24 +135,81 @@ async def buscar_contactos_por_email(email: str):
     except Exception as e:
         print(f'Error al buscar contactos por email: {e}')
         raise HTTPException(status_code=500, detail="Error al buscar contactos por email")
-    
+
+# Security setup
 security = HTTPBasic()
-security_bearer = HTTPBearer()
 
 # Conexión a la base de datos SQLite
 conn = sqlite3.connect('sql/usuarios.db')
 
-
 class TokenResponseModel(BaseModel):
     token: str
+
+def validate_credentials(conn, username, password_hash):
+    try:
+        c = conn.cursor()
+        c.execute("SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?", (username, password_hash))
+        result = c.fetchone()
+
+        if result:
+            return result[2]  # Devuelve el token almacenado en la base de datos para el usuario
+        else:
+            return None  # Credenciales no válidas
+
+    except sqlite3.Error as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error al validar las credenciales",
+        )
+
+def new_token():
+    return secrets.token_hex(128)  # Genera un nuevo token aleatorio
+
+def decrypt_token(encrypted_token):
+    return encrypted_token
+
 
 @app.get("/")
 async def root(credentials: HTTPBasicCredentials = Depends(security)):
     user = validate_credentials(conn, credentials.username, hashlib.sha512(credentials.password.encode()).hexdigest())
+    conn.close()  # Asegúrate de cerrar la conexión
     return {"message": "Token válido para el usuario: {}".format(user)}
 
-@app.get("/token/", response_model=TokenResponseModel)
-async def validate_user(credentials: HTTPBasicCredentials = Depends(security)):
+class TokenResponseModel(BaseModel):
+    token: str
+
+def cambiar_token_en_login(usuario):
+    token = str(new_token())
+    c = conn.cursor()
+    c.execute("UPDATE usuarios SET token = ? WHERE usuario = ?", (token, usuario))
+    return token
+
+async def get_user_token(usuario: str, password_hash: str):
+    try:
+        c = conn.cursor()
+        c.execute("SELECT token FROM usuarios WHERE usuario = ? AND contrasena = ?", (usuario, password_hash))
+        result = c.fetchone()
+        return result
+    except sqlite3.Error as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error al obtener token del usuario",
+        )
+
+async def login(usuario: str, contrasena: str):
+    contrasena_hash = hash_password(contrasena)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?", (usuario, contrasena_hash))
+    result = cursor.fetchone()
+
+    if result:
+        usuario_token = decrypt_token(result[2])
+        return {"message": "Inicio de sesión exitoso", "user_token": usuario_token}
+    else:
+        raise HTTPException(status_code=401, detail="Usuario no encontrado, intente de nuevo por favor")
+
+@app.get("/auth/", response_model=TokenResponseModel)
+async def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
     usuario = credentials.username
     password_hash = hashlib.sha512(credentials.password.encode()).hexdigest()
 
@@ -174,30 +225,11 @@ async def validate_user(credentials: HTTPBasicCredentials = Depends(security)):
             headers={"WWW-Authenticate": "Basic"},
         )
 
-def cambiar_token_en_login(usuario):
-    token = str(new_token())
-    c = conn.cursor()
-    c.execute("UPDATE usuarios SET token = ? WHERE usuario = ?", (token, usuario))
-    return token
-
-async def get_user_token(usuario: str, password_hash: str):
-    try:
-        c = conn.cursor()
-        c.execute("SELECT token FROM usuarios WHERE usuario = ? AND contrasena = ?", (usuario, password_hash))
-        result = c.fetchone()
-        return result
-    except sqlite3.Error as e:
-        # Manejar la excepción de manera adecuada, loggear o notificar según sea necesario
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Error al obtener token del usuario",
-        )
-
 # Nuevo endpoint para la ruta "/registro/"
 @app.post("/registro/")
 async def registro(usuario: str, contrasena: str):
     contrasena_hash = hash_password(contrasena)  # Hashear la contraseña
-    token = generate_token()  # Generar un token aleatorio
+    token = new_token()  # Generar un token aleatorio
     token_hash = hash_password(token)  # Encriptar el token
 
     # Insertar datos en la base de datos
@@ -207,71 +239,10 @@ async def registro(usuario: str, contrasena: str):
 
     return {"message": "Usuario registrado exitosamente"}
 
-# Nuevo endpoint para la ruta "/login/"
-@app.get("/login/")
-async def login(usuario: str, contrasena: str):
-    contrasena_hash = hash_password(contrasena)
-
-    # Validar el inicio de sesión consultando la base de datos
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?", (usuario, contrasena_hash))
-    result = cursor.fetchone()
-
-    # Verificar si se encontró un usuario válido en la base de datos
-    if result:
-        usuario_token = decrypt_token(result[2])  # Obtener el token del usuario y desencriptarlo
-        return {"message": "Inicio de sesión exitoso", "user_token": usuario_token}
-    else:
-        # Mostrar un mensaje de error y devolver un error HTTP
-        raise HTTPException(status_code=401, detail="Usuario no encontrado, intente de nuevo por favor")
-
-# Función para validar un token en la base de datos
-def validate_token(conn: sqlite3.Connection, token: str) -> str:
-    # Crear un cursor para ejecutar la consulta en la base de datos
-    c = conn.cursor()
-
-    # Ejecutar una consulta para obtener el usuario asociado con el token
-    c.execute("SELECT usuario FROM usuarios WHERE token = ?", (token,))
-
-    # Obtener el resultado de la consulta
-    result = c.fetchone()
-
-    # Verificar si se encontró un usuario y devolverlo, de lo contrario, lanzar una excepción
-    if result:
-        return result[0]
-    else:
-        raise HTTPException(status_code=401, detail="Token no válido")
-
-# Función para validar las credenciales de usuario en la base de datos y obtener un token
-def validate_credentials(conn: sqlite3.Connection, email: str, password_hash: str) -> str:
-    # Crear un cursor para ejecutar la consulta en la base de datos
-    c = conn.cursor()
-
-    # Ejecutar una consulta para obtener el token asociado con el usuario y la contraseña
-    c.execute("SELECT token FROM usuarios WHERE usuario = ? AND contrasena = ?", (email, password_hash))
-
-    # Obtener el resultado de la consulta
-    result = c.fetchone()
-
-    # Verificar si se encontró un token y devolverlo, de lo contrario, lanzar una excepción
-    if result:
-        return result[0]
-    else:
-        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
-
 # Función para hashear una contraseña
 def hash_password(password: str) -> str:
     # Utilizar el hash SHA-512 para la contraseña
     return hashlib.sha512(password.encode()).hexdigest()
-
-# Función para generar un token aleatorio
-def generate_token() -> str:
-    return secrets.token_hex(32)
-
-# Función para desencriptar un token
-def decrypt_token(encrypted_token: str) -> str:
-    return encrypted_token
-
 
 if __name__ == "__main__":
     import uvicorn
